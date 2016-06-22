d414a52783b356316d937cea28bdc1e884ab8cfc
Marcel Miranda <m@reaktivo.com>
2016-06-22 22:23:42 +0200

    Added toBool function to convert string 'true' and 'false' to actual values

--- helpers/string.js
+++ helpers/string.js
@@ -2,5 +2,15 @@ module.exports = {
   countNumberOfLines: function countNumberOfLines(str) {
     str = String(str).trim();
     return str.length ? str.split(/\r\n|\r|\n/).length : 0;
+  },
+  toBool: function(value) {
+    if (typeof value === 'string') {
+      if (value === 'true') {
+        return true;
+      } else if (value === 'false') {
+        return false;
+      }
+    }
+    return !!value;
   }
 }
