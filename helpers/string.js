7182f545f48d14b7021bd95df484f986a0eb9200
Marcel Miranda <m@reaktivo.com>
2016-06-23 11:13:05 +0200

    Add parseSessionId fn to string helper

--- helpers/string.js
+++ helpers/string.js
@@ -1,8 +1,18 @@
 module.exports = {
+
   countNumberOfLines: function countNumberOfLines(str) {
     str = String(str).trim();
     return str.length ? str.split(/\r\n|\r|\n/).length : 0;
   },
+
+  parseSessionId: function parseSessionId(sessionStr) {
+    return sessionStr
+      .split("JSESSIONID=")
+      .pop()
+      .split(";")
+      .shift();
+  },
+
   toBool: function(value) {
     if (typeof value === 'string') {
       if (value === 'true') {
